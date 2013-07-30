d5e6e4c79596a3cc16ce63ed4d6fa6b4b86a5287
Samuele Maccazzola <samuele.maccazzola@azcom.it>
2013-07-30 14:08:01 +0200

    Add libQtXmlPatterns
    Signed-off-by: Samuele Maccazzola <samuele.maccazzola@azcom.it>

--- bt_MyOpenBag_Scripts/LINUX_MyOpenSimulator.sh
+++ bt_MyOpenBag_Scripts/LINUX_MyOpenSimulator.sh
@@ -6,6 +6,8 @@ cp /usr/local/Trolltech/Qt-4.8.4/lib/libQtNetwork.so VDK/bin/.
 ln -s libQtNetwork.so VDK/bin/libQtNetwork.so.4
 cp /usr/local/Trolltech/Qt-4.8.4/lib/libQtCore.so VDK/bin/.
 ln -s libQtCore.so VDK/bin/libQtCore.so.4
+cp /usr/local/Trolltech/Qt-4.8.4/lib/libQtXmlPatterns.so VDK/bin/.
+ln -s libQtXmlPatterns.so VDK/bin/libQtXmlPatterns.so.4
 cp /usr/local/Trolltech/Qt-4.8.4/lib/libQtGui.so VDK/bin/.
 ln -s libQtGui.so VDK/bin/libQtGui.so.4
 cp ../BIN_libplant/liblibplant.so.2.1.3 VDK/bin/.
